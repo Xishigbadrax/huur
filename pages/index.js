45ec83b5f5b7b6c895d676ebd8a3cde421293db4
Xishigbadrax <xishigbadrax.enhtur@gmail.com>
2021-07-19 11:32:35 +0800

    aaa

--- pages/index.js
+++ pages/index.js
@@ -1,99 +1,113 @@
 import { useState } from "react";
 import css from "../styles/Home.module.css";
 
-const aimags = [
+const medeelel = [
   {
+    isAimag: true,
     id: 1,
     name: "Аймаг-1",
   },
   {
+    isAimag: true,
     id: 2,
     name: "Аймаг-2",
   },
-];
 
-const sums = [
   {
-    id: 1,
+    isSum: true,
+    id: 3,
     name: "А1-Сум1",
     aimagName: "Аймаг-1",
   },
   {
-    id: 2,
+    isSum: true,
+    id: 4,
     name: "А1-Сум2",
     aimagName: "Аймаг-1",
   },
 
   {
-    id: 3,
+    isSum: true,
+    id: 5,
     name: "А2-Сум1",
     aimagName: "Аймаг-2",
   },
   {
-    id: 4,
+    isSum: true,
+    id: 6,
     name: "А2-Сум2",
     aimagName: "Аймаг-2",
   },
-];
 
-const bags = [
   {
-    id: 1,
+    isBag: true,
+    id: 7,
     name: "А1-С1-Баг1",
     sumName: "А1-Сум1",
   },
   {
-    id: 2,
+    isBag: true,
+    id: 8,
     name: "А1-С1-Баг2",
     sumName: "А1-Сум1",
   },
   {
-    id: 3,
+    isBag: true,
+    id: 9,
     name: "А1-С1-Баг3",
     sumName: "А1-Сум1",
   },
   {
-    id: 4,
+    isBag: true,
+    id: 10,
     name: "А1-С2-Баг1",
     sumName: "А1-Сум2",
   },
   {
-    id: 5,
+    isBag: true,
+    id: 11,
     name: "А1-С2-Баг2",
     sumName: "А1-Сум2",
   },
   {
-    id: 6,
+    isBag: true,
+    id: 12,
     name: "А1-С2-Баг3",
     sumName: "А1-Сум2",
   },
   {
-    id: 7,
+    isBag: true,
+    id: 13,
     name: "А2-С1-Баг1",
     sumName: "А2-Сум1",
   },
   {
-    id: 8,
+    isBag: true,
+    id: 14,
     name: "А2-С1-Баг2",
     sumName: "А2-Сум1",
   },
   {
-    id: 9,
+    isBag: true,
+    id: 15,
     name: "А2-С1-Баг3",
     sumName: "А2-Сум1",
   },
   {
-    id: 10,
+    isBag: true,
+    id: 16,
     name: "А2-С2-Баг1",
     sumName: "А2-Сум2",
   },
   {
-    id: 11,
+    isBag: true,
+    id: 17,
     name: "А2-С2-Баг2",
     sumName: "А2-Сум2",
   },
   {
-    id: 12,
+    isBag: true,
+    id: 18,
     name: "А2-С2-Баг3",
     sumName: "А2-Сум2",
   },
@@ -114,16 +128,18 @@ export default function Home() {
         <select onChange={(e) => setAimagNer(e.target.value)}>
           <option></option>
 
-          {aimags.map((aimag) => {
-            return <option key={aimag.id}>{aimag.name}</option>;
-          })}
+          {medeelel
+            .filter((aimag) => aimag.isAimag)
+            .map((aimag) => {
+              return <option key={aimag.id}>{aimag.name}</option>;
+            })}
         </select>
         <label>Сум:</label>
         <select onChange={(e) => setSumNer(e.target.value)}>
           <option></option>
 
-          {sums
-            .filter((su) => su.aimagName === AimagNer)
+          {medeelel
+            .filter((sum) => sum.aimagName === AimagNer && sum.isSum === true)
             .map((sum) => {
               return <option key={sum.id}>{sum.name}</option>;
             })}
@@ -132,8 +148,8 @@ export default function Home() {
         <select onChange={(e) => setBagNer(e.target.value)}>
           <option></option>
 
-          {bags
-            .filter((ba) => ba.sumName === SumNer)
+          {medeelel
+            .filter((bag) => bag.sumName === SumNer && bag.isBag === true)
             .map((bag) => {
               return <option key={bag.id}>{bag.name}</option>;
             })}
@@ -143,5 +159,12 @@ export default function Home() {
         </button>
       </form>
     </div>
+    // <div>
+    // {medeelel
+    //   .filter((ba) => ba.isAimag === true && ba.id === 1 )
+    //   .map((bag) => {
+    //     return <option key={bag.id}>{bag.name}</option>;
+    //   })}
+    // </div>
   );
 }
